c2741aa5a427c6cfa1d7ecab8087fc4a159a2c6b
Godefroy Borduas <git@godefroy.ca>
2022-09-01 14:57:17 -0400

    Ajout des points d'exclamation

--- triangle.py
+++ triangle.py
@@ -2,4 +2,4 @@ if __name__ == '__main__':
     if input("Souhaitez avoir l'aire du triangle (O/N) ? ") == 'O':
         print("Un triangle de 2m par 2m a une aire de 2m^2")
     else :
-        print("Dommage")
+        print("Dommage !!!!!!")
